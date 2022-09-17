31de6db304825cf06904bea2f21683189a77860c
Khush Tated <khushtated@gmail.com>
2022-09-17 18:38:53 -0400

    Thinking about fringe

--- AStar.py
+++ AStar.py
@@ -1,6 +1,9 @@
 import math 
+import heapq
 
 class AStar:
+    closed = set()
+    fringe = []
     def __init__(self, rows, cols, blockSize, start, end, nodes, blocked): 
         self.rows = rows
         self.cols = cols
@@ -10,7 +13,10 @@ class AStar:
         self.start = start
         self.end = end
 
-    
+        # We should also add start to the fringe. What should we be heapifying based off of though?
+        # https://stackoverflow.com/questions/11989178/how-to-heapify-by-field-from-custom-objects
+
+
         
     def h(self, sx, sy): 
         sx_goal = self.end[0]
--- Node.py
+++ Node.py
@@ -4,3 +4,7 @@ class Node:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+    
+    # We have this function for Node comparison in sets
+    def __hash__(self):
+        return hash((self.x, self.y))
